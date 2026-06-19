f025b8c26eb79f1ea41786bf57e4a8a53a42de91
duartex_ <mayckonkennedy877@gmail.com>
2026-06-19 14:23:38 -0300

    repositório final

--- beck/api/app.py
+++ beck/api/app.py
@@ -9,6 +9,7 @@ app = FastAPI(title="Escola Tecno Brasilia", version="1.0.0", redirect_slashes=F
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://site-cursos.netlify.app"
 ]
 
 app.add_middleware(
